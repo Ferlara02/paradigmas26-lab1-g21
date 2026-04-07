d37402f9de79dfebcb6a1ab2851fad8a2176699d
Luca Lorenzatti <31829436+Ifs26@users.noreply.github.com>
2026-04-07 00:11:46 -0300

    Feat: se implementó función para contar frecuencias de palabras

--- src/main/scala/FileIO.scala
+++ src/main/scala/FileIO.scala
@@ -57,4 +57,58 @@ object FileIO {
         None
     }
   }
+  
+  // Ejercicio 5
+  // stopwords se pasan como parametros para permitir agregar o quitar palabras facilmente
+  def wordsFrequency(allPosts: List[(String, List[Post])], stopwords: Set[String]) : List[(String,Seq[(String,Int)])] = {
+
+    /*De cada lista de post extraemos las palabras y las guardamos como 
+    (subreddit, lista de palabras)*/
+    val wordsPerSubreddit = allPosts.map{
+      case (subreddit, postsList) => 
+        //Extraemos todas las palabras de los posts
+        val upperWords = postsList.flatMap{
+          case (_, _, selftext) =>
+            selftext.split("[,*/:'’.)\\(!?\\s)]+").filter( word => 
+              word.nonEmpty && 
+              word.head.isUpper && // Si va primero y llega string vacía salta excepción
+              !stopwords.contains(word.toLowerCase)
+              )
+        }
+        (subreddit, upperWords)
+    }
+
+    /* Contamos las palabras de cada subrredit 
+    Ahora el tipo será List[(String,Map[String,Int])]*/
+    val subredditFrequencies = wordsPerSubreddit.map{
+      case (subreddit, wordList) =>
+        /*Grupos de de cada palabra y sus veces repetidas*/
+        val wordGroups = wordList.groupBy(identity) 
+        /*Mapa de frecuencias Map("palabra" -> Int de repeticiones)*/
+        val frequencies = wordGroups.map { 
+          case (word, frequency) =>
+            (word, frequency.size)
+        }
+        (subreddit, frequencies)
+    }
+
+    /* Por último ordenamos de mayor a menor las frecuencias de cada subreddit
+    Ahora el tipo será List[(String,Seq[String,Int])]*/
+    val sortedFrequencies = subredditFrequencies.map{
+      case (subreddit, frequencies) =>
+        /* Para usar sortBy primero transformo mapa */
+        val frequenciesSorted = frequencies.toSeq.sortBy(t => -t._2)
+        (subreddit, frequenciesSorted)
+    }
+
+    sortedFrequencies
+
+    /* Uso del resultado de wordsFrequency:
+      Devuelve List[(subreddit, Seq[(palabra, frecuencia)])] ordenado de mayor a menor.
+      Ejemplo de acceso:
+        val result = wordsFrequency(allPosts, stopwords)
+        result.find(_._1 == "Scala")        // busca el subreddit "Scala"
+          .map(_._2.take(10))               // toma las 10 palabras más frecuentes
+    */
+  }
 }
